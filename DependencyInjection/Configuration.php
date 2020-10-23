4012d300176918d621496ff6b145495920ac9a0e
Burak Bolat <burak.bolat@metglobal.com>
2020-10-23 10:37:23 +0300

    Remove deprecated root node in configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,9 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('doctrine_cache_invalidation');
-
+        $treeBuilder = new TreeBuilder('doctrine_cache_invalidation');
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
